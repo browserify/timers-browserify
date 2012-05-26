04ac4745d4c53c3008a4b94220ace50f08e67435
J. Ryan Stinnett <jryans@gmail.com>
2012-05-25 22:17:34 -0500

    Add naive impl

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-var net = module.exports;
+// DOM APIs, for completeness
 
-net = require('./lib/node/net');
+exports.setTimeout = setTimeout;
+exports.clearTimeout = clearTimeout;
+exports.setInterval = setInterval;
+exports.clearInterval = clearInterval;
 
-/* net.Socket = function () {
-  console.log('Socket!');
-} */
+// TODO: Change to more effiecient list approach used in Node.js
+// For now, we just implement the APIs using the primitives above.
+
+exports.enroll = function(item, delay) {
+  item._timeoutID = setTimeout(item._onTimeout, delay);
+};
+
+exports.unenroll = function(item) {
+  clearTimeout(item._timeoutID);
+};
+
+exports.active = function(item) {
+  // our naive impl doesn't care (correctness is still preserved)
+};
